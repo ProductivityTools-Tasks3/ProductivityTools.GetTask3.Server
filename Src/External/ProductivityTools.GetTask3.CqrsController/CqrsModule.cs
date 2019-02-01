c96780a280ac5b8e88d6df49a0050430d13385fe
pwujczyk <pwujczyk@ecovadis.com>
2019-02-01 22:01:09 +0100

    cqrs controller

--- Src/External/ProductivityTools.GetTask3.CqrsController/CqrsModule.cs
+++ Src/External/ProductivityTools.GetTask3.CqrsController/CqrsModule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProductivityTools.GetTask3.CqrsController.Registration;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,9 +9,29 @@ namespace ProductivityTools.GetTask3.CqrsController
     {
         public string Name;
 
+        Dictionary<string, CqrsOperationRegistration> registartions = new Dictionary<string, CqrsOperationRegistration>();
+
         public CqrsModule(string module)
         {
             Name = module.ToLower();
+        }
+
+        protected void WireUpQuery<TRequest,TQuery,TResponse,TQueryResult>(Roles roles=null)
+        {
+            string queryName = GetOperationName<TRequest>();
+            var queryRegistration = new QueryRegistration(
+                typeof(TRequest),
+                typeof(TQuery),
+                typeof(TQueryResult),
+                typeof(TQueryResult),
+                roles);
+            registartions.Add(queryName, queryRegistration);
+        }
+
+        private string GetOperationName<TRequest>()
+        {
+            string r=nameof(TRequest).Replace("Request", string.Empty).ToLower();
+            return r;
         }
     }
 }
