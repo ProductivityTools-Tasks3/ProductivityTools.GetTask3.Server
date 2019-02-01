ad4efe4ca47a3cb66ba33e2dbdd5dd582dccd997
Pawel Wujczyk <pwujczyk@hotmail.com>
2019-02-01 16:40:08 +0100

    inital commit

---
+++ Src/External/ProductivityTools.GetTask3.CqrsController/CqrsExetensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ProductivityTools.GetTask3.CqrsController
+{
+    public class CqrsExetensions
+    {
+        public static void AddCqrs(this IServiceCollection services, params CqrsModule[] modules)
+        {
+            var registry = new CqrsRegistry();
+        }
+    }
+}
