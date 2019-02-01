8e879b6e7a2bd0a80f11b35c1b01bcbbe87c5373
pwujczyk <pwujczyk@ecovadis.com>
2019-02-01 16:40:08 +0100

    inital commit

---
+++ Src/External/ProductivityTools.GetTask3.CqrsController/CqrsModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.GetTask3.CqrsController
+{
+    public abstract class CqrsModule
+    {
+        public string Name;
+
+        public CqrsModule(string module)
+        {
+            Name = module.ToLower();
+        }
+    }
+}
---
+++ Src/Gateways/ProductivityTools.GetTask3.WebApi/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ProductivityTools.GetTask3.WebApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        IHostingEnvironment Environment;
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            Environment = env;
+        }
+        
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, )
+        {
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
