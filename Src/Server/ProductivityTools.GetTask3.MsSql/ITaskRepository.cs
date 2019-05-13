25348f15e1a3352622f213c0026eb760be6d0796
pwujczyk <pwujczyk@ecovadis.com>
2019-05-13 14:22:53 +0200

    .

---
+++ Src/Server/ProductivityTools.GetTask3.MsSql/ITaskRepository.cs
@@ -0,0 +1,9 @@
+﻿using ProductivityTools.GetTask3.Domain;
+
+namespace ProductivityTools.GetTask3.MsSql
+{
+    public interface ITaskRepository
+    {
+        Bag GetStructure();
+    }
+}
---
+++ Src/Server/ProductivityTools.GetTask3.MsSql/TaskRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductivityTools.GetTask3.Domain;
+using System;
+
+namespace ProductivityTools.GetTask3.MsSql
+{
+    public class TaskRepository : ITaskRepository
+    {
+        //pw: make it nice repository
+        public Bag GetStructure()
+        {
+            Bag root = new Bag("GetTask3", BagType.GList);
+            return root;
+        }
+    }
+}
