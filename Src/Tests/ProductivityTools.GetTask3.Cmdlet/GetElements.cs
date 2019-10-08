dfb897c2c6693f6af28e3057796f2d4d0eceee95
Pawel Wujczyk <pwujczyk@hotmail.com>
2019-10-08 09:34:41 +0200

    correct unit test

--- Src/Tests/ProductivityTools.GetTask3.Cmdlet/GetElements.cs
+++ Src/Tests/ProductivityTools.GetTask3.Cmdlet/GetElements.cs
@@ -16,20 +16,26 @@ namespace ProductivityTools.GetTask3.Cmdlet
     }
 
     [TestClass]
-    public class UnitTest1
+    public class GetElements
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetElementListCheckOrder()
         {
             var structure = new Contract.ElementView();
             structure.Elements = new System.Collections.Generic.List<Contract.ElementView>();
-            structure.Elements.Add(new Contract.ElementView() { Type = CoreObjects.ElementType.Task });
+            structure.Elements.Add(new Contract.ElementView() { ElementId = 11, Name = "First", Type = CoreObjects.ElementType.Task });
+            structure.Elements.Add(new Contract.ElementView() { ElementId = 12, Type = CoreObjects.ElementType.Task });
 
             ISessionMetaDataProvider sessionMetaDataProvider = new SessionMetaDataProviderTest();
             var taskRepository = new Moq.Mock<ITaskRepository>();
             taskRepository.Setup(i => i.GetStructure(Moq.It.IsAny<int?>())).Returns(structure);
             var task = new App.Task(sessionMetaDataProvider, taskRepository.Object);
+
+
             var x = task.Elements.ToList();
+            Assert.AreEqual(2, x.Count);
+            Assert.AreEqual("First", x[0].Element.Name);
+            Assert.AreEqual(1, x[1].SessionElement.Order);
         }
     }
 }
