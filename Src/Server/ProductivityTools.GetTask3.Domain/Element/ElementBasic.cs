47cf9be770278bb01f916f2324e4315de9d863de
Pawel Wujczyk <pwujczyk@hotmail.com>
2019-10-31 09:20:13 +0100

    change tomato view for tomato tray

---
+++ Src/Server/ProductivityTools.GetTask3.Domain/Element/ElementBasic.cs
@@ -0,0 +1,16 @@
+﻿using ProductivityTools.GetTask3.CoreObjects;
+using ProductivityTools.GetTask3.CoreObjects.Tomato;
+using ProductivityTools.GetTask3.Domain.Events;
+using ProductivityTools.GetTask3.Domain.Policy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.GetTask3.Domain
+{
+    public class ElementBasic : BaseEntity
+    {
+       
+    
+    }
+}
