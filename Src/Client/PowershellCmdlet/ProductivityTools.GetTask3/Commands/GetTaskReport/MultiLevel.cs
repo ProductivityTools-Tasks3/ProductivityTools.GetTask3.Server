48ddd618d0c1e0e4bad9f0ae4c1dfb3a266600d9
pwujczyk <pwujczyk@ecovadis.com>
2019-11-15 13:48:09 +0100

    task raport

---
+++ Src/Client/PowershellCmdlet/ProductivityTools.GetTask3/Commands/GetTaskReport/MultiLevel.cs
@@ -0,0 +1,80 @@
+﻿using ProductivityTools.ConsoleColors;
+using ProductivityTools.GetTask3.Commands.GetTask.Formatters;
+using ProductivityTools.GetTask3.Contract;
+using ProductivityTools.GetTask3.Domain;
+using ProductivityTools.GetTask3.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleColor = ProductivityTools.ConsoleColors.ConsoleColor;
+
+namespace ProductivityTools.GetTask3.Commands.GetTaskReport
+{
+    class MultiLevel : PSCmdlet.PSCommandPT<GetTaskReportCmdlet>
+    {
+        App.Task TaskStructure { get; set; }
+
+        public MultiLevel(GetTaskReportCmdlet cmdletType) : base(cmdletType)
+        {
+        }
+
+        protected override bool Condition => true;
+
+        protected override void Invoke()
+        {
+            this.TaskStructure = TaskStructureFactory.Get(this.Cmdlet);
+            if (TaskStructure.CurrentElement == null)
+            {
+                WriteOutput("No task found");
+            }
+            else
+            {
+                string parent = $"+[{TaskStructure.CurrentElement.Name}]";
+                DisplayNode(parent, TaskStructure.Structure.Elements);
+            }
+        }
+
+        private void DisplayNode(string parentpath, IEnumerable<ElementView> elements)
+        {
+            DisplayList(parentpath, elements, CoreObjects.ElementType.TaskBag);
+            DisplayList(parentpath, elements, CoreObjects.ElementType.Task);
+        }
+
+        public void DisplayList(string parentPath, IEnumerable<ElementView> elements, CoreObjects.ElementType type)
+        {
+            foreach (var element in elements.Where(x => x.Type == type))
+            {
+                WriteToScreen(parentPath, element);
+                if (element.Elements.Any())
+                {
+                    string parent = parentPath + $".[{element.Name}]";
+                    DisplayNode(parent, element.Elements);
+                }
+            }
+        }
+
+        private static void WriteToScreen(string parentPath, ElementView element)
+        {
+            var colorString = FormatRow(parentPath, element);
+            ConsoleColor.WriteInColor(colorString);
+            ResetColor();
+        }
+
+        private static void ResetColor()
+        {
+            Console.ResetColor();
+        }
+
+        private static ColorString FormatRow(string parentpath, ElementView element)
+        {
+            var result = new ColorString();
+            new Parent().Format(result, parentpath);
+            new Category().Format(result, element);
+            new Tomato().Format(result, element);
+            new ItemName().Format(result, element);
+            return result;
+        }
+    }
+}
--- Src/Common/ProductivityTools.GetTask3.CommonConfiguration/Consts.cs
+++ Src/Common/ProductivityTools.GetTask3.CommonConfiguration/Consts.cs
@@ -13,7 +13,7 @@ namespace ProductivityTools.GetTask3.CommonConfiguration
         public const string FinishTomato = "FinishTomato";
         public const string GetTomato = "GetTomato";
 
-        public const string HttpAddress = @"http://localhost:5503/";
+        public const string HttpAddress = @"http://localhost:5504/";
         public static string EndpointAddress
         {
             get
