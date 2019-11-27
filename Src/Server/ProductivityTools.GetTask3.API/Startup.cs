143eb51c53674c62f60073264c179dc5d9e40984
Pawel Wujczyk <pwujczyk@hotmail.com>
2019-11-27 22:40:41 +0100

    server

--- Src/Server/ProductivityTools.GetTask3.API/Startup.cs
+++ Src/Server/ProductivityTools.GetTask3.API/Startup.cs
@@ -49,7 +49,7 @@ namespace ProductivityTools.GetTask3.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
