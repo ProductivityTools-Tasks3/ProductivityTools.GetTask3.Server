86f9b322e141d0fe90f95dd58ce6e7a550d84d95
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-01-06 20:17:46 +0100

    cors

--- Src/Server/ProductivityTools.GetTask3.API/Startup.cs
+++ Src/Server/ProductivityTools.GetTask3.API/Startup.cs
@@ -54,7 +54,7 @@ namespace ProductivityTools.GetTask3.API
                 options.AddPolicy(MyAllowSpecificOrigins,
                     builder =>
                     {
-                        builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+                        builder.WithOrigins("http://localhost:3000", "https://task3web.z13.web.core.windows.net/").AllowAnyMethod().AllowAnyHeader();
                     });
             });
 
@@ -95,6 +95,7 @@ namespace ProductivityTools.GetTask3.API
 
             //app.UseHttpsRedirection();
             app.UseCors(MyAllowSpecificOrigins);
+
             //pw: signalR
             app.ConfigureSignalR();
 
