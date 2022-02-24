ba08a2c8853e1af1d9999fc1e7b7c9375e15662b
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-02-24 22:55:36 +0100

    init elements

---
+++ ProductivityTools.GetTask3.Server.DbUp/Scripts/009.InitElements.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO [GetTask3].[gt].[Element] (Name,Type,Status,Created) VALUES ('root',2,0,GETDATE())
+INSERT INTO [GetTask3].[gt].[Element] (Name,Type,Status,Created,ParentId) VALUES ('pwujczyk',2,0,GETDATE(),(select top 1 elementId from gt.Element))
