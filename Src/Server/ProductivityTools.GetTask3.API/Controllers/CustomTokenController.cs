f9d51f52e2b81a6f80219d1952573237e041e0c6
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-06-16 07:36:27 +0200

    custom token controller

---
+++ Src/Server/ProductivityTools.GetTask3.API/Controllers/CustomTokenController.cs
@@ -0,0 +1,19 @@
+﻿using FirebaseAdmin.Auth;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.GetTask3.API.Controllers
+{
+    public class CustomTokenController : Controller
+    {
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<string> GetToken()
+        {
+            var uid = "cmdlet";
+            string customToken=await FirebaseAuth.DefaultInstance.CreateCustomTokenAsync(uid);
+            return customToken;
+        }
+    }
+}
