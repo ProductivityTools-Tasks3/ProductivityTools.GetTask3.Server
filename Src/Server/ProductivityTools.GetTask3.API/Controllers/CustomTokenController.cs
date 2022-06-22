b216b4f5822d1b4f174991bff07c19f44d7a977e
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-06-22 07:37:47 +0200

    token controller

--- Src/Server/ProductivityTools.GetTask3.API/Controllers/CustomTokenController.cs
+++ Src/Server/ProductivityTools.GetTask3.API/Controllers/CustomTokenController.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 
 namespace ProductivityTools.GetTask3.API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class CustomTokenController : Controller
     {
 
