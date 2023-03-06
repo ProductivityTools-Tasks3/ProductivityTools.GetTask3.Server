6fceab274760890c1cec6f5821c7e4389baf6366
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-03-06 18:32:26 +0100

    validate ownership

---
+++ ProductivityTools.GetTask3.Server.DbUp/Scripts/010.ValidateOwnership.sql
@@ -0,0 +1,27 @@
+﻿USE [PTMeetings]
+GO
+
+/****** Object:  UserDefinedFunction [jl].[GetTreePath]    Script Date: 06.03.2023 18:26:25 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [gt].[ValidateOwnership] (@TreeId INT, @Name VARCHAR(100))
+    RETURNS BOOL AS
+    BEGIN
+	    DECLARE @ParentId INT
+	    
+        SELECT [ElementId],[ParentId],[Name],[Type] 
+        FROM [GetTask3].[gt].[Element] where ElementId=@TreeId
+
+        
+
+
+	                            RETURN TRUE
+                            END
+                            
+GO
+
+
